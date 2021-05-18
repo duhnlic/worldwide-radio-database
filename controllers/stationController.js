4697ba12b660f2ed1db737b9e7e36b4ed8f2493d
duhnlic <bpstew22@gmail.com>
2021-05-18 17:41:55 -0500

    removed REDACTED for bebugging

--- controllers/stationController.js
+++ controllers/stationController.js
@@ -24,7 +24,7 @@ router.get('/', async (req, res) => {
     }
 })
 // Create
-router.post('/', auth, async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const createdStation = await Station.create(req.body)
         res.status(200).json(createdStation)
@@ -46,7 +46,7 @@ router.get('/:id', async (req, res) => {
     }
 })
 // Update
-router.put('/:id', auth, async (req, res) => {
+router.put('/:id', async (req, res) => {
     try {
         const updatedStation = await Station.findByIdAndUpdate(req.params.id, req.body, { new: true } )
         res.status(200).json(updatedStation);
@@ -57,7 +57,7 @@ router.put('/:id', auth, async (req, res) => {
     }
 })
 // Delete
-router.delete('/:id', auth, async (req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
         const deletedStation = await Station.findByIdAndDelete(req.params.id);
         res.status(200).json(deletedStation);
